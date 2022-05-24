31e9e755e60ceb67ab358269214ed63b3fe5ada8
rersozlu <ersozlu.rafi@gmail.com>
2022-05-24 05:19:59 +0300

    changed name

--- src/App.jsx
+++ src/App.jsx
@@ -58,7 +58,7 @@ function App() {
         </div>
         <div className="candidate">
           <img src="/imgs/caner.png" alt="caner" />
-          <label htmlFor="caner">CANER BUDAK</label>
+          <label htmlFor="caner">ZAFER BUDAK</label>
           <input
             onClick={(e) => setChoice(e.target.id)}
             type="radio"
